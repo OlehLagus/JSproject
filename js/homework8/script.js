bc9548cd3b8a506d3b1659f54c17e96df6f7e1c4
Oleh <oleh.lahus@gmail.com>
2022-09-02 20:32:49 +0300

    homework8

--- js/homework8/script.js
+++ js/homework8/script.js
@@ -2,11 +2,31 @@
 
 
 
-let json= '{"price": 100}'
+let json = '{"user":"Oleh", "price": "100"}'
 
 try {
-  let userPrice = JSON.parse(json);
-  console.log(userPrice.price); 
+  let usersInfo = JSON.parse(json);
+  console.log(usersInfo.price)
+  return usersInfo.price
+  
 } catch (erorr) {
-  console.log("Ошибка")
+  console.log(erorr.name)
+  console.log(erorr.message)
+  console.log(erorr.stack)
+  // throw new Erorr()
+}
+
+
+const userPrice = {price: 100};
+
+if (userPrice && userPrice.price) {
+  console.log(userPrice.price)
+}
+
+
+
+const userFirstPrice = {price: 100};
+
+if (userFirstPrice?.price) {
+  console.log(userFirstPrice.price)
 }
