ecdf2db6cd31c58082ec2df685336db86d3046a8
Oleh <oleh.lahus@gmail.com>
2022-09-06 20:37:00 +0300

    homework9.2

--- js/homework9/script.js
+++ js/homework9/script.js
@@ -17,17 +17,34 @@ console.log(newArrayResult)
 
 
 const greetUser = new Map;
-let userBob = "Bob";
-let userJane = "Jane"
-
 greetUser.set(userBob, "Hello Bob nice to see you")
 greetUser.set(userJane, "How your kids doing Jane?")
+
 
 console.log(greetUser.get(userBob))
 console.log(greetUser.get(userJane))
 
-
-
 // 3. Создать функцию по превращению Map в object или object в Map в зависимости от того что было передано в функцию.
 
+
+const map = new Map()
+map.set('banana', 1);
+map.set('orange', 2);
+map.set('meat', 4);
+
+
+const obj = { banana: 1, orange: 2, meat: 4 }
+
+
+
+
+function getInfo (element) {
+   if (element === map) {
+    return Object.fromEntries(element)
+   } else 
+    return new Map(Object.entries(element))
+}
+    
+
+console.log(getInfo(map))
 
